9b44a47d1f981c10933092715c70aee4d24f4fdc
caomin1204418950 <1204418950@qq.com>
2017-12-02 03:33:14 +0800

    '首页切图完成'

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,105 @@
 //index.js
 //获取应用实例
-const app = getApp()
+
 
 Page({
   data: {
-    motto: '',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      },1000)
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+  routers:[
+    
+      { 
+          id:"0",
+        name: '转账',  
+        url: '',  
+        icon: '../../images/转账.png'  
+      },  
+      { 
+          id: "1",          
+        name: '信用卡还款',  
+        url: '',  
+        icon: '../../images/信用卡还款.png'  
+      },  
+      {  
+          id: "2",
+          
+        name: '充值中心',  
+        url: '',  
+        icon: '../../images/充值中心.png'  
+      },  
+       {  
+           id: "3",
+        name: '余额宝',  
+        url:'',  
+        icon:'../../images/余额宝.png'   
+      },  
+       {  
+           id: "4",
+        name: '淘票票',  
+        url:'',  
+        icon:'../../images/淘票票.png'   
+      },  
+       {  
+           id: "5",
+        name: '滴滴出行',  
+        url:'',  
+        icon:'../../images/滴滴出行.png'   
+      },  
+       {  
+           id: "6",
+        name: '校园工作',  
+        url:'',  
+        icon:'../../images/校园工作.png'   
+      },  
+       {  
+           id: "7",
+        name: '芝麻信用',  
+        url:'',  
+        icon:'../../images/芝麻信用.png'   
+      },  
+      {  
+          id: "8",
+        name: '火车票机票',  
+        url:'',  
+        icon:'../../images/火车票.png'   
+      }, 
+      {  
+          id: "9",
+        name: '记账本',  
+        url:'',  
+        icon:'../../images/记账本.png'   
+      }, 
+      {  
+          id: "10",
+        name: '饿了么',  
+        url:'',  
+        icon:'../../images/饿了么.png'   
+      }, 
+       {  
+           id: "11",
+        name: '更多',  
+        url:'',  
+        icon:'../../images/more.png'   
+      }  
+  
+],
+tips:[ 
+    {
+        url:'https://github.com/caomin1204418950/resource/blob/master/test1.png?raw=true',
+    },
+    {
+        url:'https://github.com/caomin1204418950/resource/blob/master/test2.png?raw=true'
     }
+
+]
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  intopage:function(e){
+      console.log(e);
+  },
+
+  onLoad: function (options) {
+  
+     
   }
 })
+
+  //事件处理函数
+  
--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -5,14 +5,75 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    routers: [  
+      {  
+        name: 'T1',  
+        url: '',  
+        icon: '../../images/shouye.png'  
+      },  
+      {  
+        name: 'T2',  
+        url: '',  
+        icon: ''  
+      },  
+      {  
+        name: 'T3',  
+        url: '',  
+        icon: ''  
+      },  
+       {  
+        name: 'T4',  
+        url:'',  
+        icon:''   
+      },  
+       {  
+        name: 'T5',  
+        url:'',  
+        icon:''   
+      },  
+       {  
+        name: 'T6',  
+        url:'',  
+        icon:''   
+      },  
+       {  
+        name: 'T7',  
+        url:'',  
+        icon:''   
+      },  
+       {  
+        name: 'T8',  
+        url:'',  
+        icon:''   
+      },  
+      {  
+        name: 'T8',  
+        url:'',  
+        icon:''   
+      }, 
+      {  
+        name: 'T8',  
+        url:'',  
+        icon:''   
+      }, 
+      {  
+        name: 'T8',  
+        url:'',  
+        icon:''   
+      }, 
+       {  
+        name: 'T9',  
+        url:'',  
+        icon:''   
+      }  
+    ]  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+ 
   },
 
   /**
@@ -26,7 +87,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
